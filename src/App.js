60b4a9026dca6a40eb8b2b345649c489746c596c
thirdybernardo <54013921+thirdybernardo@users.noreply.github.com>
2020-10-21 14:33:27 +0800

    cc
    cc

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React, { useState } from "react";
 import "./App.css";
 import Axios from "axios";
  
-function App() {
+const App = () => {
   const [query, setQuery] = useState("");
-  const [recipes, setRecipes] = useState([]);
-  const [alert, setAlert] = useState("");
+ 
 
   const APP_ID = "f7c2bed8";
   const APP_KEY = "883bd1f59bb883d3c65b8af0919f7722";
 
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const getData = async () => {
-	  const result = await Axios.get(url);
+	const result = await Axios.get(url);
 	   console.log(result);
 	  
    /*  if (query !== "") {
@@ -28,8 +27,8 @@ function App() {
     } else {
       setAlert("Please fill the form");
     } */
-  };
-
+  
+  }
   const onChange = e => setQuery(e.target.value);
 
   const onSubmit = e => {
@@ -41,7 +40,6 @@ function App() {
     <div className="App">
      <h1>Food Searching App</h1>
        <form onSubmit={onSubmit} className="search-form">
-        {alert !== "" && <Alert alert={alert} />}
         <input
           type="text"
           name="query"
